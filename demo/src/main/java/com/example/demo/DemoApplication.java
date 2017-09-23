e1bdcdd31239159908110bcff56bb95583b7da2e
uday <udaykumar1113@gmail.com>
2017-09-23 12:34:13 -0400

    Commented lines in main class

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.tool.MathFunctions;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		
+		MathFunctions function=new MathFunctions();
+		//System.out.println(function.add(15, 10));
+		System.out.println(function.sub(15,10));
+	}
+}
---
+++ demo/src/main/java/com/example/demo/tool/MathFunctions.java
@@ -0,0 +1,14 @@
+package com.example.demo.tool;
+
+public class MathFunctions 
+{
+	public int add(int var1, int var2)
+	{
+		return var1+var2;
+	}
+	
+	public int sub(int var1, int var2)
+	{
+		return var1-var2;
+	}
+}
