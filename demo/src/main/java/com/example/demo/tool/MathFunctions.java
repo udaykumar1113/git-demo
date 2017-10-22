1a11415876657071b055be1976a558e8cccee223
uday <udaykumar1113@gmail.com>
2017-10-21 20:18:12 -0400

    added caps and space

--- demo/src/main/java/com/example/demo/tool/MathFunctions.java
+++ demo/src/main/java/com/example/demo/tool/MathFunctions.java
@@ -4,13 +4,13 @@ public class MathFunctions
 {
 	public int add(int var1, int var2)
 	{
-		System.out.println("THIS IS AN ADD FUNCTION");
+		System.out.println("THIS IS AN ADD FUNCTION ");
 		return var1+var2;
 	}
 	
 	public int sub(int var1, int var2)
 	{
-		System.out.println("THIS IS AN SUB FUNCTION");
+		System.out.println("THIS IS AN SUB FUNCTION ");
 		return var1-var2;
 	}
 }
